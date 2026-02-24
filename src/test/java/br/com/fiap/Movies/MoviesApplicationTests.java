73b31d48f6b575d20baa5ec90cc6f721bf6dfe61
Moisés Barsoti <moises.barsot2@gmail.com>
2026-02-24 11:08:42 -0300

    chore: feito a descompactação do Spring

---
+++ src/test/java/br/com/fiap/Movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.Movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
