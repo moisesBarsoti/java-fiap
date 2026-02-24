24b9651df41b88810f0e6029c4981dd8e9fd88e5
Moisés Barsoti <moises.barsot2@gmail.com>
2026-02-24 11:40:36 -0300

    feat(MovieController): adicionado uma requisição teste
    Usado três anotações Controller, RequestMapping e ResponseBody

---
+++ src/main/java/br/com/fiap/Movies/controllers/MovieController.java
@@ -0,0 +1,20 @@
+package br.com.fiap.Movies.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MovieController {
+
+    @RequestMapping(
+            path = "/",
+            method = RequestMethod.GET,
+            produces = "application/json"
+    )
+    @ResponseBody
+    public String healthCheck() {
+        return "ok";
+    }
+}
